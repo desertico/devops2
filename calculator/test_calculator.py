92aadf06617ce3d9dcf913818e3e02e7cc8122bf
Raul <desertico@gmail.com>
2021-12-18 03:56:39 +0000

    nuevo cambio 3

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -1,4 +1,8 @@
-import calculator:
+
+
+
+import calculator
+
 
 class TestCalculator:
 
@@ -7,6 +11,9 @@ class TestCalculator:
 
     def test_addition2(self):
         assert 5 == calculator.add(2, 3)
+    
+    def test_divide(self):
+        assert 2 == calculator.add(4, 2)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
